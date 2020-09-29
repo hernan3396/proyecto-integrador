2470bc98dc56cf67d9f82d9a931d6f9a600241c1
hernan3396 <hernan3396@gmail.com>
2020-09-29 20:56:13 -0300

    changed index dispatch line 13 to userLoginSucces

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import store from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import "marx-css/css/marx.min.css";
-import { signupUserSuccess } from "./features/user/userSlice";
+import { userLoginSuccess } from "./features/user/userSlice";
 import api from "./app/api";
 
 if (localStorage.getItem("token")) {
-  store.dispatch(signupUserSuccess());
+  store.dispatch(userLoginSuccess());
   api.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
     "token"
   )}`;
